b216510b2b6f193f96573172e70393d93011a9eb
wangzeng <2662735959@qq.com>
2018-07-10 12:07:33 +0800

    vue-app

---
+++ src/vuex/store.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+const state = {
+  photoList: []
+};
+
+const mutations = {
+  setPhotoList(state, status) {
+    state.photoList = status;
+  }
+};
+
+// 相当于computed
+const getters = {
+
+};
+
+const actions = {
+  setPhotoList({
+    commit
+  }, status) {
+    commit('setPhotoList', status);
+  }
+};
+
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions
+});
+export default  new Vue.State(
+  {
+    state
+  }
+);
---
+++ vuecli/config/prod.env.js
@@ -0,0 +1,5 @@
+//生产文件配置
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"'
+}
